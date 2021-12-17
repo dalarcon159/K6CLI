244689bbe4b332299a9414b6f3db3052a03c1ca0
dalarcon159@gmail.com <dalarcon159@gmail.com>
2021-12-17 02:57:17 -0500

    commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dalarcon159/K6CLI
+
+go 1.15
+
+require github.com/spf13/cobra v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "k6maker/cmd"
+
+func main() {
+	cmd.Execute()
+}
